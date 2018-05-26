e4a80d50c23cdda4002a3868343dba428c53aed6
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2018-05-26 12:26:56 +0000

    Update TestComsRPiCode.py
    Added A handshake code for raspberry pi

--- TestComsRPiCode.py
+++ TestComsRPiCode.py
@@ -1,10 +1,22 @@
 import serial
+import time
 ser = serial.Serial("/dev/ttyS0", 9600, timeout=1)
 
-ser.write("testing")
-try:
-    while 1:
-        response = ser.readline()
-        print(response)
-except KeyboardInterrupt:
-    ser.close()
+# Declare the Packet Codes for the Packet Type
+ACK = b"\x00"
+NACK = b"\x01"
+HELLO = b"\x02"
+
+# Handshake Flags
+handshake_flag = True
+
+# Handshake condition
+while handshake_flag:
+    time.sleep(1)                       # 1 second pause timing
+    ser.write(HELLO)                    # Send Hello to Arduino
+    str = ser.readline()                # Read Arduino's response
+    reply = int(str)
+    if (reply == 0):                    # Check if the reply is an ACK
+        handshake_flag = False
+        ser.write(ACK)                  # If true, change flag and ACK
+print('Handshake completed')
