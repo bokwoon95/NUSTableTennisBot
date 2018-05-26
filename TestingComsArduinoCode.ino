8868dee1c0b9890a45fd6b0b63dc816d4e01f58c
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2018-05-26 12:27:32 +0000

    Update TestingComsArduinoCode.ino
    Updated Handshake code for Arduino Uno

--- TestingComsArduinoCode.ino
+++ TestingComsArduinoCode.ino
@@ -1,13 +1,36 @@
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Packet code
+int16_t ACK = 0, NAK = 1, HELLO = 2, READ = 3, WRITE = 4, DATA_RESP = 5;
+
+// Method to Handshake with the RaspberryPI
+void handshake() {
+  int reply;
+  int handshake_flag = 1;
+  
+  while (handshake_flag == 1) {
+    if (Serial.available()) {
+      reply = Serial.read();
+      if (reply == HELLO) {
+        Serial.write(ACK);
+      }
+      if (reply == ACK) {
+        handshake_flag = 0;
+      }
+    }
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  handshake();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    int incoming = Serial.read();
-    Serial.print("character recieved: ");
-    Serial.print(incoming, DEC);
-  }
 }
