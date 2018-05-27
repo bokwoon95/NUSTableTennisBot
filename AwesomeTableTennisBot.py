2792ed84932aae1054f2986be0aaaa8cd326510d
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2018-05-27 03:22:20 +0000

    Update AwesomeTableTennisBot.py
    Ensure that there are some working features

--- AwesomeTableTennisBot.py
+++ AwesomeTableTennisBot.py
@@ -1,11 +1,11 @@
 import requests
 import json
-from time import sleep
-from telegram.ext.dispatcher import run_async
+import telegram
 
 token = "577090085:AAFVzW4Z3dG7W0vcL3jdrW7pYFIGwvHrNCc" #Token of your bot
 url = 'https://api.telegram.org/bot{}/'.format(token)
 machineState = True
+isSomeonePlaying = True
 
 # Get Own User
 # Currently Used
@@ -13,6 +13,7 @@ def getme():
     res=requests.get(url+"getme")
     data = res.json()
     username = data['result']['username']
+    return username;
 
 # Updates the New User
 def get_updates(offset = None):
@@ -23,7 +24,6 @@ def get_updates(offset = None):
 
         res = requests.get(URL)
         while (res.status_code !=200 or len(res.json()['result'])== 0):
-            sleep(1)
             res = requests.get(URL)
         print(res.url)
         return res.json()
@@ -43,11 +43,12 @@ def get_last_id_text(updates):
     last_update = get_last(updates)
     chat_id =last_update['message']['chat']['id']
     update_id = last_update['update_id']
+    first_name = last_update['message']['from']['first_name']
     try:
         text = last_update['message']['text']
     except:
         text = ''
-    return chat_id,text,update_id
+    return chat_id,text,update_id,first_name
 
 # Send a message to the user
 def send_message(chat_id, text,reply_markup=None):
@@ -80,7 +81,7 @@ def machineChecker(chat_id, update_id):
         commands = ['Yes', 'No']
         reply_markup = reply_markup_maker(commands)
         send_message(chat_id,message,reply_markup)
-        chat_id,text,update_id = get_last_id_text(get_updates())
+        chat_id,text,update_id,first_name = get_last_id_text(get_updates(update_id+1))
         if text.lower() == 'yes':
             message = "Prepare to rumble"
             send_message(chat_id,message)
@@ -93,78 +94,88 @@ def machineChecker(chat_id, update_id):
         message = "There is no player as of now. Come and train with me!"
         send_message(chat_id,message)
     return
+    
+# Checks to see if there is a player nearby
+def playByYourself(chat_id, text, update_id):
+    return chat_id, text, update_id
 
-def end(chat_id,text,update_id):
+def end(chat_id,text,update_id,first_name):
     message = 'Do you wanna end?'
     reply_markup = reply_markup_maker(['Yes','No'])
     send_message(chat_id,message,reply_markup)
-    chat_id,text,update_id = get_last_id_text(get_updates())
-
-    if text.lower() =='yes':
-        return 'y'
+    chat_id,text,update_id,first_name = get_last_id_text(get_updates(update_id+1))
+    if text.lower() == 'yes':
+        text = "end"
+        return chat_id,text,update_id,first_name
     elif text.lower() == 'no':
-        return 'n'
-    else:
-        spawnNewUser(update_id)
+        return chat_id,text,update_id,first_name
 
-# Menu
-# Called: When a user starts the bot
-# Returns: none
-def menu(chat_id,text,update_id):
-    print('User in main menu')
-    message = 'Select'
-    commands = ['Machine Checker','Speed Controller']
+def sendGreenLightMessageToArduino():
+    return True
+        
+# Send Green Light to Arduino
+# Called: When the bot starts
+def sendGreenLight(chat_id, text, update_id,first_name):
+    print('Sending Green Light')
+    sendGreenLightMessageToArduino()
+    message = 'Playing user has been informed that you are coming!'
+    send_message(chat_id,message)
+    chat_id, text, update_id,first_name = get_last_id_text(get_updates())
+    return chat_id, text, update_id,first_name
+
+# Ask to user o join session
+# Called: main()
+# Returns: Boolean
+def askToJoinSession(chat_id,text,update_id,first_name):
+    print('Ask User if they want to join')
+    message = 'Hi a user is playing, do you want to join?'
+    commands = ['Yes','No']
     reply_markup = reply_markup_maker(commands)
     send_message(chat_id,message,reply_markup)
-    chat_id,text,update_id = get_last_id_text(get_updates())
-
-    if text.lower() == 'machine checker':
-        machineChecker(chat_id, update_id)
-    elif text.lower() == 'speed controller':
-        print('User should be at speed controller')
-        message = 'you are controlling your speed now'
-    else:
-        spawnNewUser(update_id)
-    return
+    chat_id,text,update_id,first_name = get_last_id_text(get_updates(update_id+1))
+    return chat_id,text,update_id,first_name
 
 # Starts Bot
 # Called: When the bot starts
-def start(chat_id):
+def start(chat_id,text,update_id,first_name):
     print('Users at start')
-    message = 'Wanna Start?'
-    reply_markup=reply_markup_maker(['Start'])
+    message = 'Hi '+ first_name +' wanna start?'
+    reply_markup = reply_markup_maker(['Start'])
     send_message(chat_id,message,reply_markup)
-    chat_id,text,update_id = get_last_id_text(get_updates())
+    chat_id,text,update_id,first_name = get_last_id_text(get_updates(update_id+1))
     
     if text.lower() == 'start':
-        return 'n'
-    else:
-        spawnNewUser(update_id)
-    return
-
-# Create New User
-# Called: Whenever a new user joins
-# Returns: none
-def spawnNewUser(update_id):
-    chat_id,text,update_id = get_last_id_text(get_updates(update_id+1))
-    menu(chat_id,text,update_id)
-    sleep(0.5)    
+        return chat_id,text,update_id,first_name
 
 # Main Function
 # Called: When user starts the bot
 # Return: None
 def main():
-    chat_id, text, update_id = get_last_id_text(get_updates())
-    text = start(chat_id)
-    while text.lower() !='y':
-        menu(chat_id,text,update_id)
-        text = end(chat_id,text,update_id)
+    chat_id,text,update_id,first_name = get_last_id_text(get_updates())
+    # Until the User Presses Start
+    chat_id,text,update_id,first_name = start(chat_id,text,update_id,first_name)    
+    while text.lower() != 'start':
+        chat_id,text,update_id,first_name = start(chat_id,text,update_id,first_name)
     
+    while text.lower() != 'end':
+        while isSomeonePlaying != True:
+           chat_id, text, update_id,first_name = playByYourself(chat_id, text, update_id,first_name)
+        
+        chat_id, text, update_id,first_name = askToJoinSession(chat_id, text, update_id,first_name)
+
+        if text.lower() == 'yes':
+            chat_id, text, update_id,first_name = sendGreenLight(chat_id, text, update_id,first_name)
+        
+        chat_id,text,update_id,first_name = end(chat_id,text,update_id,first_name)
+    
+    message = 'Thank you for using Awesome Table Tennis Bot'
+    send_message(chat_id,message)
 
 # Start
 if __name__ == '__main__':
     print('Hi, NUSTableTennisBot now launching...')
     try:
-        main()
+        while True:
+            main()
     except KeyboardInterrupt:
          exit()
